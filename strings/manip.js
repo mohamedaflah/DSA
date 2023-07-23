6b4425301db6f0ef5c2bcea662972842886f06ef
Aflu <mohamedaflah186@gmail.com>
2023-07-23 17:58:51 +0530

    another one

--- strings/manip.js
+++ strings/manip.js
@@ -1,28 +1,35 @@
+//                 Changing String to ASCII
+//                 Changing ASCII to String
+
 // const a = 'a';
-// console.log(a.charCodeAt(0)); 
+// console.log(a.charCodeAt(0));
 
 // const charCode = 97;
-// console.log(String.fromCharCode(charCode)); 
+// console.log(String.fromCharCode(charCode));
 // var msyst="aflu";
 
-function manip(str,n){
-    let newStr="";
-    for(let i=0;i<str.length;i++){
-        let ne=str[i].charCodeAt(0);
-        if(n>26){
-            n=n-26;
-        }
-        if(str[i].charCodeAt(0)<122){
-            ne=ne+n;
-            newStr+=String.fromCharCode(ne);
-        }else{
-            let rev=(str[i].charCodeAt(0)-122)+96;
-            newStr+=String.fromCharCode(rev+2)
-        }
-        
-
+function manip(str, n) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    let ne = str[i].charCodeAt(0);
+    if (n > 26) {
+      n = n - 26;
     }
-    return newStr;
+    if (str[i].charCodeAt(0) < 122) {
+      if (n == 2 && str[i] == "Y") {
+        newStr += "A";
+      } else if (n==2 && str[i] == "y") {
+        newStr += "a";
+      } else {
+        ne = ne + n;
+        newStr += String.fromCharCode(ne);
+      }
+    } else {
+      let rev = str[i].charCodeAt(0) - 122 + 96;
+      newStr += String.fromCharCode(rev + 2);
+    }
+  }
+  return newStr;
 }
-const ans=manip("haz",3);
-console.log(ans)
+const ans = manip("hai", 2);
+console.log(ans);
